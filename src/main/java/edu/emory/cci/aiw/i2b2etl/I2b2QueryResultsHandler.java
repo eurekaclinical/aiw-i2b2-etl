a281c1f6e651e3dfe2ccb87ed280156c34f11b7f
Andrew Post <arpost@emory.edu>
2014-12-10 15:24:53 -0500

    Removed duplicate dependency on protempa-dsb-relationaldb. Fixed use of StringUtils to use commons-lang3 instead of commons-lang.

--- src/main/java/edu/emory/cci/aiw/i2b2etl/I2b2QueryResultsHandler.java
+++ src/main/java/edu/emory/cci/aiw/i2b2etl/I2b2QueryResultsHandler.java
@@ -77,7 +77,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.protempa.DataSource;
 import org.protempa.backend.dsb.DataSourceBackend;
 import org.protempa.backend.ksb.KnowledgeSourceBackend;
